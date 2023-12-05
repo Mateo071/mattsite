cb88220cea77558f76e981341e6c75aedac7b93c
Mateo071 <floresmateo2000@gmail.com>
2023-12-05 15:05:01 -0500

    contact page started

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,23 @@ import Services from './pages/Services'
 import Schedule from './pages/Schedule'
 import Contact from './pages/Contact'
 
-function App() {
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+const App = () => {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyB7kZPQUR0T3Qe7Bh-AblcPZKO4vGq6nSA",
+    authDomain: "mattsite-faa5b.firebaseapp.com",
+    projectId: "mattsite-faa5b",
+    storageBucket: "mattsite-faa5b.appspot.com",
+    messagingSenderId: "121870305131",
+    appId: "1:121870305131:web:2a472f4248c03d06a8ae2e",
+    measurementId: "G-BP0JZ2M73G"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
   return (
     <main className='bg-slate-300/20 h-full'>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,49 @@
 const Contact = () => {
   return (
-    <section className='w-full h-screen relative'>
-      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-        Contact
+    <section className='relative flex lg:flex-row flex-col max-container h-[100vh]'>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>Get in Touch</h1>
+
+        <form
+          className='w-full flex flex-col gap-7 mt-14'
+        >
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input
+              type='text'
+              name='name'
+              className='input'
+              placeholder='John'
+              required
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Email
+            <input
+              type='email'
+              name='email'
+              className='input'
+              placeholder='john@gmail.com'
+              required
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Your Message
+            <input
+              name='message'
+              rows={4}
+              className='textarea'
+              placeholder='Let me know how I can help you!'
+              required
+            />
+          </label>
+          <button
+            type='submit'
+            className='btn'
+          >
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   )
